fa22f18a0960830a657a7d89c941300b1a00a4e1
ph18633 <ph18633@IT070742.users.bris.ac.uk>
2019-09-18 11:19:33 +0100

    Result of initial code review

--- CDMConverter/src/main/java/uk/ac/bristol/CDMConverter/Encoding/FileSource.java
+++ CDMConverter/src/main/java/uk/ac/bristol/CDMConverter/Encoding/FileSource.java
@@ -7,17 +7,8 @@ public class FileSource {
     @objid ("db0ed2bb-cf61-4945-a090-e545f76dedd9")
     private String filePath;
 
-    @objid ("b729a3fc-4d3c-4842-91ce-5ca35b7a9230")
-    public FileConnection fileConnection;
-
     @objid ("45663cfe-ffc5-42d2-8f21-5a17309493cd")
     public FileSource(String filePath) {
         this.filePath = filePath;
     }
-
-    @objid ("ccbd0d38-c227-45cc-b8fa-e3a7a7c4789f")
-    public void close() {
-        // TODO
-    }
-
 }
--- CDMConverter/src/main/java/uk/ac/bristol/CDMConverter/Encoding/OMOPComponents/OMOPProcedureOccurrence.java
+++ CDMConverter/src/main/java/uk/ac/bristol/CDMConverter/Encoding/OMOPComponents/OMOPProcedureOccurrence.java
@@ -5,16 +5,16 @@ import com.modeliosoft.modelio.javadesigner.annotations.objid;
 @objid ("9285b7ec-b8c4-49d1-9a3c-aa2b43747a02")
 public class OMOPProcedureOccurrence extends OMOPComponent {
     @objid ("a4a2b191-c24b-4103-8d9c-2c6140ad3bd6")
-    public int procedureConceptId;
+    private int procedureConceptId;
 
     @objid ("fc802714-30d9-4c69-bd47-60d66293ffe1")
-    public int procedureSourceConceptId;
+    private int procedureSourceConceptId;
 
     @objid ("95314ab9-56cf-471f-924e-f5718e2931a0")
-    public String procedureName;
+    private String procedureName;
 
     @objid ("91acd316-1fca-4b91-a11e-07f495f7c56e")
-    public String procedureType;
+    private String procedureType;
 
     @objid ("8b136601-769a-4948-85aa-8cdbaeaab3b7")
     public OMOPProcedureOccurrence(int pk) {
